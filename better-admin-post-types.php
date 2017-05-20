28263a94268dcdd601d696ed81fa8add422bf0fb
Mark Wilkinson <mark@highrise.digital>
2017-05-20 20:54:33 +0100

    Remove double empty line in comment

--- better-admin-post-types.php
+++ better-admin-post-types.php
@@ -21,7 +21,6 @@
  * This is an add-on for WordPress
  * http://wordpress.org/
  *
- *
  * **********************************************************************
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
